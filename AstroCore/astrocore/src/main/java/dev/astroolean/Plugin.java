646c30d899c066fc923cabc1e17cdf6124c46feb
Astroolean <162177701+Astroolean@users.noreply.github.com>
2024-10-31 17:43:55 -0400

    Update Plugin.java
    /astrocore - to enable/disable the plugin
    /hello - for the hello command
    
    As in /astrocore when used disabled the plugin to where commands such as /hello cannot be used. Then just re-enable the plugin via /astrocore again to allow the use of the plugin again.

--- AstroCore/astrocore/src/main/java/dev/astroolean/Plugin.java
+++ AstroCore/astrocore/src/main/java/dev/astroolean/Plugin.java
@@ -5,28 +5,50 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.CommandExecutor;
 
 /*
- * astrocore java plugin
+ * AstroCore java plugin
  */
 public class Plugin extends JavaPlugin
 {
-  private static final Logger LOGGER=Logger.getLogger("astrocore");
+    private static final Logger LOGGER = Logger.getLogger("AstroCore");
+    private boolean enabled = true; // Variable to keep track of enabled/disabled status
 
-  @Override
-  public void onEnable()
-  {
-    LOGGER.info("astrocore enabled");
-    // Register the command executor for the "hello" command
-    // Ensure the command is defined in plugin.yml
-    System.out.println("My first plugin has started");
-    this.getCommand("hello").setExecutor(new HelloCommand());
-  }
+    @Override
+    public void onEnable()
+    {
+        LOGGER.info("AstroCore enabled");
+        System.out.println("My first plugin has started");
+    
+        // Register commands
+        this.getCommand("AstroCore").setExecutor(new AstroCoreCommand(this));
+        this.getCommand("hello").setExecutor(new HelloCommand(this)); // Pass the plugin instance
+    }
 
-  @Override
-  public void onDisable()
-  {
-    LOGGER.info("astrocore disabled");
-    System.out.println("My first plugin has started");
-  }
+    @Override
+    public void onDisable()
+    {
+        LOGGER.info("AstroCore disabled");
+        System.out.println("AstroCore plugin has stopped");
+    }
+
+    // Method to toggle plugin status
+    public void togglePlugin()
+    {
+        enabled = !enabled;
+        if (enabled)
+        {
+            LOGGER.info("AstroCore enabled");
+        }
+        else
+        {
+            LOGGER.info("AstroCore disabled");
+        }
+    }
+
+    public boolean isEnabledCustom()
+    {
+        return enabled;
+    }
 }
