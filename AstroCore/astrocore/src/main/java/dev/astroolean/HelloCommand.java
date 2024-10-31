bf1a3a5ac8cc8e86f28662b4ace99b82ec5f6032
Astroolean <162177701+Astroolean@users.noreply.github.com>
2024-10-31 17:44:23 -0400

    Update HelloCommand.java
    /astrocore - to enable/disable the plugin
    /hello - for the hello command
    
    As in /astrocore when used disabled the plugin to where commands such as /hello cannot be used. Then just re-enable the plugin via /astrocore again to allow the use of the plugin again.

--- AstroCore/astrocore/src/main/java/dev/astroolean/HelloCommand.java
+++ AstroCore/astrocore/src/main/java/dev/astroolean/HelloCommand.java
@@ -1,23 +1,33 @@
 package dev.astroolean;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
-public class HelloCommand implements CommandExecutor {
+public class HelloCommand implements CommandExecutor
+{
+    private final Plugin plugin;
+
+    public HelloCommand(Plugin plugin)
+    {
+        this.plugin = plugin;
+    }
+
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // Check if the command sender is a player
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            // Send a message to the player
-            player.sendMessage("Hello, " + player.getName() + "!");
-            return true; // Command executed successfully
-        } else {
-            // If the sender is not a player, send a message to the console
-            sender.sendMessage("This command can only be run by a player.");
-            return false; // Command not executed successfully
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+    {
+        if (!plugin.isEnabledCustom()) // Check if the plugin is "disabled"
+        {
+            sender.sendMessage(ChatColor.RED + "AstroCore plugin is currently disabled.");
+            return true;
         }
+
+        if (command.getName().equalsIgnoreCase("hello"))
+        {
+            sender.sendMessage(ChatColor.YELLOW + "Hello, " + sender.getName() + "! Welcome to the server!");
+            return true;
+        }
+        return false;
     }
 }
