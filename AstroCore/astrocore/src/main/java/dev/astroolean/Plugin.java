e7b792595a4831a7553f02bb8e83827d1af12bac
Astroolean <162177701+Astroolean@users.noreply.github.com>
2024-11-01 00:55:07 -0400

    Update Plugin.java
    More Commands...
    - /godmode | a way for you to be invincible.
    - /help | directs you to the suicide hotline.
    - /t | Opens the Time GUI
    - /w | Opens the Weather GUI
    - /gm | Opens the Gamemode GUI
    - /smoke | Smoke weed in order to gain an effect... (hopefully custom model data in future)
    - /snort | Snort cocaine in order to gain an effect... (hopefully custom model data in future)
    - /cc | To clear the fucking chat...
    
    Many bugs but working as if it has one leg; be patient my code is special as in built with several missing chromosomes. Expect issues...

--- AstroCore/astrocore/src/main/java/dev/astroolean/Plugin.java
+++ AstroCore/astrocore/src/main/java/dev/astroolean/Plugin.java
@@ -1,11 +1,10 @@
 package dev.astroolean;
 
 import java.util.logging.Logger;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.plugin.java.JavaPlugin;
 
 /*
  * AstroCore java plugin
@@ -19,13 +18,49 @@ public class Plugin extends JavaPlugin
     public void onEnable()
     {
         LOGGER.info("AstroCore enabled");
-        System.out.println("My first plugin has started");
-    
+        LOGGER.info("""
+           
+            ____________________________
+           |                            |
+           |      AstroCore Plugin      |
+           |            V1.1            |
+           |____________________________|
+           
+           My first plugin has started...
+
+           class AstrooleanSignature:
+               def __init__(self, NAME="Astroolean"):
+                   self.NAME = NAME
+               def Signature(self):
+                   Astro = "Astro - The online name ive always went by."
+                   Boolean = "Boolean - Embracing the simplicity of true/false."
+                   return f"/nSignature:/n{Astro}/n{Boolean}/nAll-in-all its just Astroolean."
+           Astroolean = AstrooleanSignature()
+           print(Astroolean.Signature())
+
+        """);    
         // Register commands
-        this.getCommand("AstroCore").setExecutor(new AstroCoreCommand(this));
-        this.getCommand("hello").setExecutor(new HelloCommand(this)); // Pass the plugin instance
+        registerCommand("AstroCore", new AstroCoreCommand(this));
+        registerCommand("hello", new HelloCommand(this));
+        registerCommand("help", new HelpCommand(this));
+        registerCommand("snort", new CocaineCommand(this));
+        registerCommand("smoke", new WeedCommand(this));
+        registerCommand("gm", new GamemodeCommand(this));
+        registerCommand("t", new TimeCommand(this));
+        registerCommand("w", new WeatherCommand(this));
+        registerCommand("cc", new ClearChatCommand(this));
+        registerCommand("god", new GodCommand(this));
+    
     }
 
+private void registerCommand(String commandName, CommandExecutor executor) {
+    PluginCommand command = this.getCommand(commandName);
+    if (command != null) {
+        command.setExecutor(executor);
+    } else {
+        LOGGER.warning(String.format("Command %s not found. Ensure it is defined in plugin.yml.", commandName));
+    }
+}
     @Override
     public void onDisable()
     {
